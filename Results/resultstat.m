bc6933cc8d1ddbbbb761f776909d36a5a5868f00
tirtharajdash <dashtirtharaj@gmail.com>
2021-01-15 00:29:21 +0530

    new results

--- Results/resultstat.m
+++ Results/resultstat.m
@@ -2,12 +2,18 @@ BotGNN = dlmread('~/tdash/BotGNN/Results/BotGNN.csv',',',1,1);
 BotGNN_AB = dlmread('~/tdash/BotGNN/Results/BotGNN_AB.csv',',',1,1);
 GNN = dlmread('~/tdash/VEGNN/Results/GNN.csv',',',1,1);
 VEGNN = dlmread('~/tdash/VEGNN/Results/VEGNN.csv',',',1,1);
-DRM = load('~/tdash/Basic_DRM/Results/DRM.csv');
-DRM_abfr = load('~/tdash/Basic_DRM/Results/DRM_abfr.csv');
+DRM = dlmread('~/tdash/Basic_DRM/Results/DRM.csv',',',1,1);
+DRM_abfr = dlmread('~/tdash/Basic_DRM/Results/DRMabfr.csv',',',1,1);
+XGB = dlmread('~/tdash/Basic_XGB/XGBoutputs/results.csv',',',1,1);
+SVM = dlmread('~/tdash/Basic_SVM/SVCoutputs/results.csv',',',1,1);
+GPC = dlmread('~/tdash/Basic_GPC/GPCoutputs/results.csv',',',1,1);
+
 
 for i = 1:5
     %A = BotGNN_AB(:,i); B = BotGNN(:,i);
-    A = DRM_abfr(:,1); B = DRM(:,1);
+    %A = DRM(:,1); B = BotGNN(:,i);
+    A = SVM(:,1); B = BotGNN(:,i);
+	%A = XGB(:,1); B = BotGNN(:,i);
     %A = round(A,4); B = round(B,4);
     
     gt = sum(B > A);
---
+++ run_alz.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#bash run_amine.bash
+#bash run_amine_type.bash
+
+bash run_choline.bash
+bash run_choline_type.bash
